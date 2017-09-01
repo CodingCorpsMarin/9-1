f562741c7930a93ba1ac5f8921e2370b89cc9353
Louie Chen <louiehchen@gmail.com>
2017-09-01 13:25:26 -0700

    Add files via upload

---
+++ SQL/letter.rb
@@ -0,0 +1,43 @@
+class LoveLetter
+
+	def confess
+		'I love you more than all my other hoes'
+	end
+
+	def cheat
+		'I would never cheat on purpose'
+	end
+
+	def money?
+		false
+	end
+
+	def date
+		if money?
+			'FRENCH LAUNDRY'
+		else
+			'You pay'
+		end
+	end
+
+
+	def profession_of_love 
+		reasons = ["i'm desperate", "please I'm lonely", "me love you long time"]
+		reason.each do |reason|
+			"I love you because" + reason
+		end
+	end
+
+	def truth
+		"I think you are beautiful even when I'm sober" 
+	end
+
+	def sex
+		'u wan sum fuk'
+	end
+
+	def serenade
+		'LET ME SEE THAT THOOOOOOOOONNNGGG'
+	end
+
+end
